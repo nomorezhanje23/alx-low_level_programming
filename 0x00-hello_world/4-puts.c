d133d2193f8264a91a1fb2fc437335c51be16b89
nomorezhanje23 <nomorezhanje100@gmail.com>
2023-07-13 13:48:28 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line with puts function
+ * Return: 0 when the condition is true, the line will be printed
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
