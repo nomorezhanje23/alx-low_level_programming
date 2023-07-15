3e5f4d05f92d9d2bd348b8a5a2d18dff96c765c5
nomorezhanje23 <nomorezhanje100@gmail.com>
2023-07-14 19:11:42 -0700

    try

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 for a successful program
+ */
+int main(void)
+{
+int lowerCase = 'a';
+while (lowerCase <= 'z')
+{
+if (lowerCase == 'o' || lowerCase == 'q')
+{
+lowerCase++;
+}
+else
+}
+putchar(lowerCase);
+putchar('\n');
+}
+lowerCase++;
+}
+return (0);
+}
