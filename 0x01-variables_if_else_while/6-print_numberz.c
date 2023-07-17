fa4ed992d605d158d3204c274d8d842e789eb46d
nomorezhanje23 <nomorezhanje100@gmail.com>
2023-07-17 00:16:26 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 when the program is a success
+ */
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+putchar((num % 10) + '0');
+putchar('\n');
+return (0);
+}
