ca8763b8154ce1c0a987aa8afdbc603cd188eadf
nomorezhanje23 <nomorezhanje100@gmail.com>
2023-07-21 15:50:28 -0700

    12. Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -2,8 +2,22 @@
 #include "main.h"
 
 /**
- * main - prints the largest prime factor of 612852475143
+ * print_number - prints an integer
+ * @n: .input integer parameter
  * Return: always 0
  */
 
-int main(void)
+void print_number(int n)
+{
+unsigned int i = n;
+if (n < 0)
+{
+_putchar(45);
+i = -i;
+}
+if (i / 10)
+{
+print_number(i / 10);
+}
+_putchar(i % 10 + '0');
+}
