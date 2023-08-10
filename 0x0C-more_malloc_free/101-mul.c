8de631ee94df00b3d7a8fd5daa4a9fbffeb80a3f
nomorezhanje23 <nomorezhanje100@gmail.com>
2023-08-10 14:41:40 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -11,13 +11,13 @@
  */
 int is_digit(char *s)
 {
-	int b = 0;
+	int i = 0;
 
-	while (s[b])
+	while (s[i])
 	{
-		if (s[b] < '0' || s[b] > '9')
+		if (s[i] < '0' || s[i] > '9')
 			return (0);
-		b++;
+		i++;
 	}
 	return (1);
 }
@@ -29,13 +29,13 @@ int is_digit(char *s)
  */
 int _strlen(char *s)
 {
-	int b = 0;
+	int i = 0;
 
-	while (s[b] != '\0')
+	while (s[i] != '\0')
 	{
-		b++;
+		i++;
 	}
-	return (b);
+	return (i);
 }
 
 /**
@@ -56,7 +56,7 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, b, carry, digit1, digit2, *result, a = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -67,12 +67,11 @@ int main(int argc, char *argv[])
 	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (b = 0; b <= len1 + len2; b++)
-		result[b] = 0;
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
 	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
-
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
@@ -84,12 +83,12 @@ int main(int argc, char *argv[])
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-	for (b = 0; b < len - 1; b++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[b])
+		if (result[i])
 			a = 1;
 		if (a)
-			_putchar(result[b] + '0');
+			_putchar(result[i] + '0');
 	}
 	if (!a)
 		_putchar('0');
