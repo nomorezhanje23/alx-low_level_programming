63f3a29a8f7d64935206d18aab09db12bcf4c981
nomorezhanje23 <nomorezhanje100@gmail.com>
2023-08-29 01:44:05 -0700

    Free - Zimbabwe 2023

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,6 +3,7 @@
 /**
  * free_listint2 - Frees the list and sets the head to NULL.
  * @head: pointer to the head of list
+ * this code frees the nodes, realise the memory and sets head
  */
 
 void free_listint2(listint_t **head)
