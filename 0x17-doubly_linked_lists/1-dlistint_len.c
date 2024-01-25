ad7e00a9857795bbc8d7fb4340f207af195602ce
nomorezhanje23 <nomorezhanje23@gmail.com>
2024-01-25 08:51:09 -0800

    initial commit

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns number of double linked list
+ * @h: the head of the list
+ * Return: nodes number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
